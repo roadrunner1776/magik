e5f5ffe0616e9bc218c9581ae063c1812b20ed9d
Sebastiaan Speck <sebastiaan.speck@keronic.com>
2022-04-12 19:37:49 +0200

    [sw-method-docs] Add support for assignment method

--- magik-mode.el
+++ magik-mode.el
@@ -383,6 +383,8 @@ Based upon `font-lock-warning-face'"
 (defconst magik-regexp
   '(("method" .
      "^[_abstract\s|_private\s|_iter\s]*?_method.*(\\([\0-\377[:nonascii:]]*?\\))")
+    ("assignment-method" .
+     "^[_abstract\s|_private\s|_iter\s]*?_method.*<<\s?\\(.*\\)")
     ("endmethod" .
      "^\\s-*_endmethod\\s-*\\(\n\\$\\s-*\\)?$")
     ("method-argument" .
@@ -1853,6 +1855,9 @@ Argument END ..."
      ((eq major-mode 'magik-mode)
       (goto-char (point-min))
       (while (search-forward-regexp (cdr (assoc "method" magik-regexp)) nil t)
+        (magik-parse-sw-method-docs (match-string 1)))
+      (goto-char (point-min))
+      (while (search-forward-regexp (cdr (assoc "assignment-method" magik-regexp)) nil t)
         (magik-parse-sw-method-docs (match-string 1)))))))
 
 (defun magik-single-sw-method-docs ()
@@ -1864,8 +1869,12 @@ Argument END ..."
       (forward-line)
       (search-backward-regexp (cdr (assoc "method" magik-regexp)) nil t)
       (search-forward-regexp (cdr (assoc "method" magik-regexp)) nil t)
-      (unless (equal (match-string 1) nil)
-	(magik-parse-sw-method-docs (match-string 1)))))))
+      (if (not (equal (match-string 1) nil))
+	  (magik-parse-sw-method-docs (match-string 1))
+	(search-backward-regexp (cdr (assoc "assignment-method" magik-regexp)) nil t)
+	(search-forward-regexp (cdr (assoc "assignment-method" magik-regexp)) nil t)
+	(unless (equal (match-string 1) nil)
+	  (magik-parse-sw-method-docs (match-string 1))))))))
 
 (defun magik-parse-sw-method-docs (method-string)
   "Helper function for inserting sw-method-docs.
