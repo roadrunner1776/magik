e80c3052c7296ce9d07f7512745edaea93e80a71
Sebastiaan Speck <shem.speck@gmail.com>
2024-08-26 09:59:49 +0200

    [yasnippet] Switch SPC to yas-maybe-expand

--- magik-mode.el
+++ magik-mode.el
@@ -36,6 +36,7 @@
 
 (require 'compat)
 (require 'imenu)
+(require 'yasnippet)
 (require 'magik-template)
 
 (defgroup magik nil
@@ -2202,7 +2203,7 @@ closing bracket into the new \"{...}\" notation."
   (define-key magik-mode-map "\r" 'magik-newline)
   (define-key magik-mode-map "\n" 'newline)
   (define-key magik-mode-map "\t" 'magik-indent-command)
-  (define-key magik-base-mode-map " " 'magik-electric-space)
+  (define-key magik-base-mode-map " " yas-maybe-expand)
   (define-key magik-base-mode-map "#" 'magik-electric-hash)
   (define-key magik-base-mode-map "/" 'magik-electric-pragma-slash)
   (define-key magik-base-mode-map "\\" 'magik-electric-pragma-backslash)
