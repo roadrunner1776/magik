90a219bfa7078cd5e578da6f1e4f2e3877a1a613
Sebastiaan Speck <shem.speck@gmail.com>
2025-06-29 08:38:21 +0200

    [magik-yasnippet] Use current directory name to form the module/product name

--- magik-utils.el
+++ magik-utils.el
@@ -81,6 +81,10 @@ Returns a list of paths, or nil if none are found."
       (setq directory (file-name-directory (directory-file-name directory))))
     (nreverse paths)))
 
+(defun magik-utils-current-directory-name ()
+  "Return the directory name for the current buffer."
+  (file-name-nondirectory (directory-file-name (file-name-directory buffer-file-name))))
+
 (defun magik-utils-curr-word ()
   "Return the word (or part-word) before point as a string."
   (save-excursion
