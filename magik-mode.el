54da3daff23a5738c89e4c0c657f340b8124a451
roadrunner1776 <roadrunner1776>
2025-07-23 10:28:07 +0200

    [release] 0.5.1

--- magik-mode.el
+++ magik-mode.el
@@ -1,6 +1,6 @@
 ;;; magik-mode.el --- Emacs major mode for Smallworld Magik files
 
-;; Package-Version: 0.5.0
+;; Package-Version: 0.5.1
 ;; Package-Requires: ((emacs "24.4") (compat "28.1") (yasnippet "0.14.0"))
 ;; URL: https://github.com/roadrunner1776/magik
 ;; Keywords: languages
