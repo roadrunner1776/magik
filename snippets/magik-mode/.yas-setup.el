f71053df3b9e0135e9b81174d99b73c9f67ba2a6
Reinier Koffijberg <reinierkof@gmail.com>
2025-10-20 09:04:34 +0200

    resolve commments

--- snippets/magik-mode/.yas-setup.el
+++ snippets/magik-mode/.yas-setup.el
@@ -168,8 +168,8 @@ If the buffer is not visiting a file, return an empty string."
                                      (number-sequence 0 (1- (length slots)))))
              "\n")))))))
 
-(defun magik-yasnippet-after-point-empty? ()
-  "Current line after point is empty or contain only whitespaces."
+(defun magik-yasnippet--after-point-empty-p ()
+  "Return t if the line after point is empty or contain only whitespace."
   (save-excursion
     (let ((line-after-point (buffer-substring-no-properties (point) (line-end-position))))
       (string-match-p "^\\s-*$" line-after-point))))
