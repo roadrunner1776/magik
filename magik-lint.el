af03f08724a1d9f405c43252469a9e7cdf5480d0
Sebastiaan Speck <12570668+sebastiaanspeck@users.noreply.github.com>
2026-05-18 12:49:25 +0200

    Update magik-lint.el

--- magik-lint.el
+++ magik-lint.el
@@ -36,7 +36,7 @@
   :group 'magik)
 
 (defcustom magik-lint-jar-file-version
-  (or (magik-lint--latest-version) "0.11.0")
+  (or (magik-lint--latest-version) "0.12.0")
   "Version of magik-lint to use."
   :group 'magik-lint
   :type 'string)
