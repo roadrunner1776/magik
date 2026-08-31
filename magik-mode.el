5a8598f36cf8c691270125c8d6254cc0263a8e70
Sebastiaan Speck <12570668+sebastiaanspeck@users.noreply.github.com>
2026-08-31 18:47:31 +0200

    Remove unused lexical variables in magik-mode.el (#325)

--- magik-mode.el
+++ magik-mode.el
@@ -1715,12 +1715,10 @@ Argument END ..."
 (defun magik-parse-pragma ()
   "Helper function for inserting pragma."
   (let ((ending-point (line-number-at-pos))
-        (starting-point 0)
         (search-result nil))
     (save-excursion
       (search-backward-regexp "^\\$" nil t)
-      (setq starting-point (line-number-at-pos)
-            search-result (search-forward-regexp (cdr (assoc "pragma" magik-regexp)) nil t))
+      (setq search-result (search-forward-regexp (cdr (assoc "pragma" magik-regexp)) nil t))
       (when (or (and (not (equal search-result nil))
                      (< ending-point (line-number-at-pos)))
                 (equal search-result nil))
@@ -1775,7 +1773,7 @@ This function is modified from `imenu--generic-function' to basically
 provide extra control over the name that appears in the index."
 
   (let ((index-alist (list 'dummy))
-        prev-pos beg
+        beg
         (case-fold-search imenu-case-fold-search)
         (old-table (syntax-table))
         (table (copy-syntax-table (syntax-table)))
